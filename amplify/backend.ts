5f004791eaacae97deef4245983c382ad6794cef
Kazuki Matsui <kmtitmk123@gmail.com>
2025-03-04 11:57:27 +0900

    ビルドテスト

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,2 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
-defineBackend({});
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+defineBackend({
+  auth,
+  data,
+});
