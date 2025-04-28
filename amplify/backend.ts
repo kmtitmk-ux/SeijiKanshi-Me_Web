2d83c21bcce390ea9cdbf7591add13c350308c3f
Kazuki Matsui <kmtitmk123@gmail.com>
2025-04-28 15:02:03 +0900

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { firstBucket, secondBucket } from './storage/resource';
 
 
 /**
