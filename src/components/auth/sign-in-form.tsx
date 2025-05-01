e69d073f2af75e06c1d9e6b24f58e04e5e885131
Kazuki Matsui <kmtitmk123@gmail.com>
2025-05-01 09:48:04 +0900

    パスのエラー修正

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -23,8 +23,8 @@ import { authClient } from '@/lib/auth/client';
 import { useUser } from '@/hooks/use-user';
 
 const schema = zod.object({
-  email: zod.string().min(1, { message: 'Email is required' }).email(),
-  password: zod.string().min(1, { message: 'Password is required' }),
+    email: zod.string().min(1, { message: 'Email is required' }).email(),
+    password: zod.string().min(1, { message: 'Password is required' }),
 });
 
 type Values = zod.infer<typeof schema>;
@@ -32,122 +32,122 @@ type Values = zod.infer<typeof schema>;
 const defaultValues = { email: 'sofia@devias.io', password: 'Secret1' } satisfies Values;
 
 export function SignInForm(): React.JSX.Element {
-  const router = useRouter();
+    const router = useRouter();
 
-  const { checkSession } = useUser();
+    const { checkSession } = useUser();
 
-  const [showPassword, setShowPassword] = React.useState<boolean>();
+    const [showPassword, setShowPassword] = React.useState<boolean>();
 
-  const [isPending, setIsPending] = React.useState<boolean>(false);
+    const [isPending, setIsPending] = React.useState<boolean>(false);
 
-  const {
-    control,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<Values>({ defaultValues, resolver: zodResolver(schema) });
+    const {
+        control,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<Values>({ defaultValues, resolver: zodResolver(schema) });
 
-  const onSubmit = React.useCallback(
-    async (values: Values): Promise<void> => {
-      setIsPending(true);
+    const onSubmit = React.useCallback(
+        async (values: Values): Promise<void> => {
+            setIsPending(true);
 
-      const { error } = await authClient.signInWithPassword(values);
+            const { error } = await authClient.signInWithPassword(values);
 
-      if (error) {
-        setError('root', { type: 'server', message: error });
-        setIsPending(false);
-        return;
-      }
+            if (error) {
+                setError('root', { type: 'server', message: error });
+                setIsPending(false);
+                return;
+            }
 
-      // Refresh the auth state
-      await checkSession?.();
+            // Refresh the auth state
+            await checkSession?.();
 
-      // UserProvider, for this case, will not refresh the router
-      // After refresh, GuestGuard will handle the redirect
-      router.refresh();
-    },
-    [checkSession, router, setError]
-  );
+            // UserProvider, for this case, will not refresh the router
+            // After refresh, GuestGuard will handle the redirect
+            router.refresh();
+        },
+        [checkSession, router, setError]
+    );
 
-  return (
-    <Stack spacing={4}>
-      <Stack spacing={1}>
-        <Typography variant="h4">Sign in</Typography>
-        <Typography color="text.secondary" variant="body2">
-          Don&apos;t have an account?{' '}
-          <Link component={RouterLink} href={paths.auth.signUp} underline="hover" variant="subtitle2">
-            Sign up
-          </Link>
-        </Typography>
-      </Stack>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing={2}>
-          <Controller
-            control={control}
-            name="email"
-            render={({ field }) => (
-              <FormControl error={Boolean(errors.email)}>
-                <InputLabel>Email address</InputLabel>
-                <OutlinedInput {...field} label="Email address" type="email" />
-                {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
-              </FormControl>
-            )}
-          />
-          <Controller
-            control={control}
-            name="password"
-            render={({ field }) => (
-              <FormControl error={Boolean(errors.password)}>
-                <InputLabel>Password</InputLabel>
-                <OutlinedInput
-                  {...field}
-                  endAdornment={
-                    showPassword ? (
-                      <EyeIcon
-                        cursor="pointer"
-                        fontSize="var(--icon-fontSize-md)"
-                        onClick={(): void => {
-                          setShowPassword(false);
-                        }}
-                      />
-                    ) : (
-                      <EyeSlashIcon
-                        cursor="pointer"
-                        fontSize="var(--icon-fontSize-md)"
-                        onClick={(): void => {
-                          setShowPassword(true);
-                        }}
-                      />
-                    )
-                  }
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-                />
-                {errors.password ? <FormHelperText>{errors.password.message}</FormHelperText> : null}
-              </FormControl>
-            )}
-          />
-          <div>
-            <Link component={RouterLink} href={paths.auth.resetPassword} variant="subtitle2">
-              Forgot password?
-            </Link>
-          </div>
-          {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}
-          <Button disabled={isPending} type="submit" variant="contained">
-            Sign in
-          </Button>
+    return (
+        <Stack spacing={4}>
+            <Stack spacing={1}>
+                <Typography variant="h4">Sign in</Typography>
+                <Typography color="text.secondary" variant="body2">
+                    Don&apos;t have an account?{' '}
+                    <Link component={RouterLink} href={paths.auth.signUp} underline="hover" variant="subtitle2">
+                        Sign up
+                    </Link>
+                </Typography>
+            </Stack>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={2}>
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormControl error={Boolean(errors.email)}>
+                                <InputLabel>Email address</InputLabel>
+                                <OutlinedInput {...field} label="Email address" type="email" />
+                                {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
+                            </FormControl>
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormControl error={Boolean(errors.password)}>
+                                <InputLabel>Password</InputLabel>
+                                <OutlinedInput
+                                    {...field}
+                                    endAdornment={
+                                        showPassword ? (
+                                            <EyeIcon
+                                                cursor="pointer"
+                                                fontSize="var(--icon-fontSize-md)"
+                                                onClick={(): void => {
+                                                    setShowPassword(false);
+                                                }}
+                                            />
+                                        ) : (
+                                            <EyeSlashIcon
+                                                cursor="pointer"
+                                                fontSize="var(--icon-fontSize-md)"
+                                                onClick={(): void => {
+                                                    setShowPassword(true);
+                                                }}
+                                            />
+                                        )
+                                    }
+                                    label="Password"
+                                    type={showPassword ? 'text' : 'password'}
+                                />
+                                {errors.password ? <FormHelperText>{errors.password.message}</FormHelperText> : null}
+                            </FormControl>
+                        )}
+                    />
+                    <div>
+                        <Link component={RouterLink} href={paths.auth.resetPassword} variant="subtitle2">
+                            Forgot password?
+                        </Link>
+                    </div>
+                    {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}
+                    <Button disabled={isPending} type="submit" variant="contained">
+                        Sign in
+                    </Button>
+                </Stack>
+            </form>
+            <Alert color="warning">
+                Use{' '}
+                <Typography component="span" sx={{ fontWeight: 700 }} variant="inherit">
+                    sofia@devias.io
+                </Typography>{' '}
+                with password{' '}
+                <Typography component="span" sx={{ fontWeight: 700 }} variant="inherit">
+                    Secret1
+                </Typography>
+            </Alert>
         </Stack>
-      </form>
-      <Alert color="warning">
-        Use{' '}
-        <Typography component="span" sx={{ fontWeight: 700 }} variant="inherit">
-          sofia@devias.io
-        </Typography>{' '}
-        with password{' '}
-        <Typography component="span" sx={{ fontWeight: 700 }} variant="inherit">
-          Secret1
-        </Typography>
-      </Alert>
-    </Stack>
-  );
+    );
 }
--- src/paths.ts
+++ src/paths.ts
@@ -1,12 +1,12 @@
 export const paths = {
     home: '/',
-    // auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
+    auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
     dashboard: {
         population: '/dashboard',
-        // account: '/dashboard/account',
-        // customers: '/dashboard/customers',
-        // integrations: '/dashboard/integrations',
-        // settings: '/dashboard/settings',
+        account: '/dashboard/account',
+        customers: '/dashboard/customers',
+        integrations: '/dashboard/integrations',
+        settings: '/dashboard/settings',
     },
     errors: { notFound: '/errors/not-found' },
 } as const;
