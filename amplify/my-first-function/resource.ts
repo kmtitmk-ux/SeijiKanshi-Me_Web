5f8773a7f47d1bf12b0497c9f0591365ff6b1a61
Kazuki Matsui <kmtitmk123@gmail.com>
2025-06-06 16:00:33 +0900

    エラーテスト

--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const myFirstFunction = defineFunction({
     name: "my-first-function",
-    entry: "./handler.ts",
+    entry: "./handler.js",
 });
