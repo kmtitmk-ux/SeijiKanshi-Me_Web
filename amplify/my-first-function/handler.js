a9ea0aa7e82ee6c016894a451fbefe75a4a51c41
Kazuki Matsui <kmtitmk123@gmail.com>
2025-06-06 15:18:51 +0900

    エラー修正

--- amplify/my-first-function/handler.js
+++ amplify/my-first-function/handler.js
@@ -3,7 +3,7 @@ import {
     InvokeModelCommand
 } from "@aws-sdk/client-bedrock-runtime";
 
-export const handler = async () => {
+export const myFirstFunction = async () => {
     const client = new BedrockRuntimeClient({ region: "us-east-1" });
 
     const input = {
