b7d340f5d92947ed4dd58724cb813a14c8d556fd
Kazuki Matsui <kmtitmk123@gmail.com>
2025-06-18 07:59:26 +0900

    ts復活

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,8 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { myFirstFunction } from './my-first-function/resource';
+
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
