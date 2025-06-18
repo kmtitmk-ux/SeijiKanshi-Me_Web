caf0d415b9a81029e12893ecc7fe12785d6c7649
Kazuki Matsui <kmtitmk123@gmail.com>
2025-06-18 13:18:28 +0900

    .jsの修正

--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 export const myFirstFunction = defineFunction({
     name: "my-firdefineFunctionst-function",
     // runtime: 'nodejs20.x',
-    entry: "./handler.js"
+    entry: "./handler.ts"
 });
